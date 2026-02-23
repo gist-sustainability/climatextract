1ca419ae4e6d070cf4b675acb1388a428414d282
Malte Schierholz <malte.schierholz@gmail.com>
2026-02-24 00:15:12 +0100

    Revert "docs: review and improve output description"
    This reverts commit 9128034ce6f7f06ce404a6d715726c47fa3b1d90.

--- from climatextract import extract.py
+++
@@ -1,9 +0,0 @@
-from climatextract import extract
-from climatextract import extract_and_evaluate
-
-# Current interface
-# result_path = extract("data/pdfs/sato holdings_2022_report.pdf")
-# print(f"Results saved to: {result_path}")
-
-result_path = extract_and_evaluate(pdf_input="./data/pdfs/sato oyj_2022_report.pdf", gold_standard_path="./data/evaluation_dataset/gist_2025.csv")
-print(f"Results saved to: {result_path}")
